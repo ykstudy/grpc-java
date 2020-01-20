d1894d0ae34308a0fd976ef19b79a4f41a790b16
1245423598@qq.com <1245423598@qq.com>
2020-01-20 10:31:53 +0800

    CCC

--- src/test/java/cn/ykstudy/GrpcApplicationTests.java
+++ src/test/java/cn/ykstudy/GrpcApplicationTests.java
@@ -2,8 +2,6 @@ package cn.ykstudy;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 //@RunWith(SpringRunner.class)
 //@SpringBootTest
